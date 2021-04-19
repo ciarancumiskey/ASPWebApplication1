87ba1a867fec736076e15fa6fcc9830265e09bff
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2021-04-19 12:56:14 +0100

    Initial commit

---
+++ WebApplication1/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string GameName { get; set; }
+        public string GamePlatform { get; set; }
+        public int GamePrice { get; set; }
+        public string GameDescription { get; set; }
+        public Game()
+        {
+
+        }
+    }
+}
