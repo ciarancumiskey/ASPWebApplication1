d5e1c2ab68cea5c3e6a9c6b20c1f28f3e899fb05
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2021-04-20 15:57:10 +0100

    Set up database

---
+++ WebApplication1/Data/Migrations/20210420143951_initialSetup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Data.Migrations
+{
+    public partial class initialSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GameName = table.Column<string>(nullable: true),
+                    GamePlatform = table.Column<string>(nullable: true),
+                    GamePrice = table.Column<int>(nullable: false),
+                    GameDescription = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Game");
+        }
+    }
+}
